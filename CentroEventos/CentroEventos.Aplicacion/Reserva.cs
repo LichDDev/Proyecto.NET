d33deca378fb656b28d56934f184521cc1a1c756
Dylan <portaldylan12@gmail.com>
2025-05-08 02:11:37 -0300

    implementacion de clase Reserva 0.1 y enum Estado

---
+++ CentroEventos/CentroEventos.Aplicacion/Reserva.cs
@@ -0,0 +1,11 @@
+
+namespace CentroEventos.Aplicacion;
+
+public class Reserva
+{
+    public int ID{get;}
+    public int PersonalId{get;}
+    public int EventoDeportivoId{get;}
+    public DateTime FechaAltaReserva{get;}
+    public Estado EstadoAsistencia{get;}
+}
