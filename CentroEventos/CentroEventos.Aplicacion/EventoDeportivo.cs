1ca76829e965f9cfe7d9430031f3312e2068075f
Dylan <portaldylan12@gmail.com>
2025-05-08 02:05:09 -0300

    implementacion de clase EventoDeportivo 0.1

---
+++ CentroEventos/CentroEventos.Aplicacion/EventoDeportivo.cs
@@ -0,0 +1,12 @@
+﻿namespace CentroEventos.Aplicacion;
+
+public class EventoDeportivo()
+{
+    public int ID{get;} 
+    public string? Nombre{get;} 
+    public string? Descripcion{get;}
+    public DateTime FechaHoraInicio{get;}
+    public double DuracionHoras{get;}
+    public int CupoMaximo{get;}
+    public int ResponsableId{get;} 
+}
