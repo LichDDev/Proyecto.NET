b3cd1024d0f85ba2cb9f88622463087ec6b2433b
Dylan <portaldylan12@gmail.com>
2025-05-09 14:06:09 -0300

    Cambio en la entidad Reserva
    Cambie la propiedad PersonalID por personaID, en el validador tmb

--- CentroEventos/CentroEventos.Aplicacion/Entities/Reserva.cs
+++ CentroEventos/CentroEventos.Aplicacion/Entities/Reserva.cs
@@ -4,7 +4,7 @@ namespace CentroEventos.Aplicacion;
 public class Reserva
 {
     public int ID{get;set;}
-    public int PersonalId{get;set;}
+    public int PersonaId{get;set;}
     public int EventoDeportivoId{get;set;}
     public DateTime FechaAltaReserva{get;set;}
     public Estado EstadoAsistencia{get;set;}
