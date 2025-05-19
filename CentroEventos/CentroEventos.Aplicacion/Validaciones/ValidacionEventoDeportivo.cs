e9b86807ecd8118f35796f0924ad5d78280bc10b
Nicoapa23 <nicolasaparicio04@hotmail.com.ar>
2025-05-19 11:56:49 -0300

    Cambio en las validaciones//Ahora retornan un bool y un mensaje para que los useCase lanzen la excepcion si hace falta (Las excepciones especiales siguen siendo lanzadas por el validador)

--- CentroEventos/CentroEventos.Aplicacion/Validaciones/ValidacionEventoDeportivo.cs
+++ CentroEventos/CentroEventos.Aplicacion/Validaciones/ValidacionEventoDeportivo.cs
@@ -4,14 +4,18 @@ namespace CentroEventos.Aplicacion;
 
 public class ValidacionEventoDeportivo(IRepositorioPersona repo)
 {
-    public void Validar(EventoDeportivo evento)
+    public bool Validar(EventoDeportivo evento, out string message)
     {
-        string message="";
-        if(string.IsNullOrWhiteSpace(evento.Nombre)) message+= "El nombre del evento esta ausente\n";
-        if(string.IsNullOrWhiteSpace(evento.Descripcion)) message+= "La descripción del evento esta ausente\n";
-        if(evento.CupoMaximo<=0)message+= "El cupo máximo debe ser mayor que cero\n";
-        if(evento.DuracionHoras<=0)message+= "La duración debe ser mayor que cero\n";
-        if(message!="")throw new ValidacionException(message);
-        if(!repo.ExisteId(evento.ResponsableId))throw new EntidadNotFoundException("La persona responsable no existe");
+        //Se valida que no haya ningún elemento faltante
+        message = "";
+        if (string.IsNullOrWhiteSpace(evento.Nombre)) message += "El nombre del evento esta ausente\n";
+        if (string.IsNullOrWhiteSpace(evento.Descripcion)) message += "La descripción del evento esta ausente\n";
+        if (evento.CupoMaximo <= 0) message += "El cupo máximo debe ser mayor que cero\n";
+        if (evento.DuracionHoras <= 0) message += "La duración debe ser mayor que cero\n";
+        //Si la persona responsable existe, retorna si los datos son correctos, y si no existe lanza la EntidadNotFoundExeption
+        if (repo.ExisteId(evento.ResponsableId))
+            return string.IsNullOrEmpty(message);
+        else
+            throw new EntidadNotFoundException("La persona responsable no existe");
     }
 }
