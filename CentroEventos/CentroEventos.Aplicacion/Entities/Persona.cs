e89cc052c772c65110f0287ee0c4149e3d1a9e58
Nicoapa23 <nicolasaparicio04@hotmail.com.ar>
2025-05-19 12:12:25 -0300

    Implementacion de ToString() en las entidades

--- CentroEventos/CentroEventos.Aplicacion/Entities/Persona.cs
+++ CentroEventos/CentroEventos.Aplicacion/Entities/Persona.cs
@@ -4,10 +4,11 @@ namespace CentroEventos.Aplicacion;
 
 public class Persona
 {
-    public int? ID{get;set;}
-    public string? DNI {get;set;} 
-    public string? Nombre{get;set;}
-    public string? Apellido{get;set;}
-    public string? Email{get;set;}
-    public string? Telefono{get;set;}
+    public int? ID { get; set; }
+    public string? DNI { get; set; }
+    public string? Nombre { get; set; }
+    public string? Apellido { get; set; }
+    public string? Email { get; set; }
+    public string? Telefono { get; set; }
+    public override string ToString() => $"ID: {ID}, DNI: {DNI}, Nombre: {Nombre}, Apellido: {Apellido}, Email: {Email}, Telefono: {Telefono}";
 }
--- CentroEventos/CentroEventos.Aplicacion/Entities/Reserva.cs
+++ CentroEventos/CentroEventos.Aplicacion/Entities/Reserva.cs
@@ -3,9 +3,10 @@ namespace CentroEventos.Aplicacion;
 
 public class Reserva
 {
-    public int ID{get;set;}
-    public int PersonaId{get;set;}
-    public int EventoDeportivoId{get;set;}
-    public DateTime FechaAltaReserva{get;set;}
-    public Estado EstadoAsistencia{get;set;}
+    public int ID { get; set; }
+    public int PersonaId { get; set; }
+    public int EventoDeportivoId { get; set; }
+    public DateTime FechaAltaReserva { get; set; }
+    public Estado EstadoAsistencia { get; set; }
+    public override string ToString() => $"ID: {ID}, Persona ID: {PersonaId}, Evento deportivo ID: {EventoDeportivoId}, Fecha de alta de reserva: {FechaAltaReserva}, Estado de asistencia: {EstadoAsistencia}";
 }
