db61af8357e0b60a53852a085a8ff09a8926e943
Dylan <portaldylan12@gmail.com>
2025-06-16 20:14:17 -0300

    Hash 1.0

--- CentroEventos/CentroEventos.Repositorios/RepositorioUsuario.cs
+++ CentroEventos/CentroEventos.Repositorios/RepositorioUsuario.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Security.Cryptography;
+using System.Text;
 using CentroEventos.Aplicacion;
 
 namespace CentroEventos.Repositorios;
@@ -8,6 +10,8 @@ public class RepositorioUsuario() : IRepositorioUsuario
     public void AgregarUsuario(Usuario u)
     {
         using var context = new CentroDeportivoContext();
+        //le aplico le hash al la contraseña antes de agregar al usuario
+        u.Contraseña = HashPassword(u.Contraseña!= null ? u.Contraseña: "");
         context.Usuarios.Add(u);
         context.SaveChanges();
     }
@@ -29,7 +33,7 @@ public class RepositorioUsuario() : IRepositorioUsuario
         using var context = new CentroDeportivoContext();
         return context.Usuarios.ToList();
     }
-    public bool ModificarUsuario( Usuario u)
+    public bool ModificarUsuario(Usuario u)
     {
         using var context = new CentroDeportivoContext();
         var usuarioModificar = context.Usuarios.Where(a => a.ID == u.ID).SingleOrDefault();
@@ -61,5 +65,30 @@ public class RepositorioUsuario() : IRepositorioUsuario
             return usuario.Permisos == null ? false : usuario.Permisos.Contains(permiso);
         else
             return false;
+    }
+    public bool VerificarContraseña(Usuario usuario)
+    {
+        using var context = new CentroDeportivoContext();
+        //se busca al usuario por su email (idea inicial)
+        var user = context.Usuarios.Where(r => r.Email == usuario.Email).SingleOrDefault();
+        if (user != null)
+        {   
+            //se aplica el hash a la contraseña ingresada
+            string? contra= HashPassword(usuario.Contraseña == null ? "": usuario.Contraseña);
+            // se hace la comparacion con la contraseña que esta en la base de datos
+            return user.Contraseña != null ? user.Contraseña.Equals(contra) : false;
+        }
+        else
+            return false;
+    }
+    private string? HashPassword(string input)
+    {
+        using SHA256 sha256 = SHA256.Create();
+        // Convertir a bytes
+        byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+        // Obtener hash
+        byte[] hashBytes = sha256.ComputeHash(inputBytes);
+        // Convertir a string hexadecimal
+        return Convert.ToHexString(hashBytes); // .NET 5+
     }
 }
