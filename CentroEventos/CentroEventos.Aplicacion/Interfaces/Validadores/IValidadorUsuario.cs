b3493522f4e676cb95755379acf0172097adcf9e
Dylan <portaldylan12@gmail.com>
2025-06-16 17:20:50 -0300

    Correcciones  menores

--- CentroEventos/CentroEventos.Aplicacion/Interfaces/Validadores/IValidadorUsuario.cs
+++ CentroEventos/CentroEventos.Aplicacion/Interfaces/Validadores/IValidadorUsuario.cs
@@ -4,5 +4,5 @@ namespace CentroEventos.Aplicacion;
 
 public interface IValidadorUsuario
 {
-    bool ValidarDatosAusentes(int id,out string message);
+    bool ValidarDatosAusentes(Usuario id,out string message);
 }
